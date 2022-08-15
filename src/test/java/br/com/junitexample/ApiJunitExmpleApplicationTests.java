f2eba9d7c084692a0c31365cb159e5c700485d89
neresdev <neredev@gmail.com>
2022-08-15 13:39:56 -0300

    v0.0.2 - neresdev - teste da classe Main (ApiJunitExampleApplpication)

--- src/test/java/br/com/junitexample/ApiJunitExmpleApplicationTests.java
+++ src/test/java/br/com/junitexample/ApiJunitExmpleApplicationTests.java
@@ -1,6 +1,14 @@
 package br.com.junitexample;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 class ApiJunitExmpleApplicationTests {
 
-
+    @Test
+    void contextLoads(){
+        ApiJunitExmpleApplication.main(new String[] {});
+    }
 }
