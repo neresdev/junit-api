8aa70bc6f30a04710a28ca5793a3afdb90868553
neredev <neresdev@gmail.com>
2023-01-28 22:22:12 -0300

    teste para classe user service - metodo delete com sucesso

--- src/test/java/br/com/junitexample/service/impl/UserServiceImplTest.java
+++ src/test/java/br/com/junitexample/service/impl/UserServiceImplTest.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 
 @SpringBootTest
@@ -153,7 +153,12 @@ class UserServiceImplTest {
     }
 
     @Test
-    void delete() {
+    void deleteWithSuccess() {
+        when(repository.findById(anyInt())).thenReturn(optionalUser);
+        doNothing().when(repository).deleteById(anyInt());
+        service.delete(ID);
+        verify(repository, times(1)).deleteById(anyInt());
+
     }
 
     private void startUser(){
