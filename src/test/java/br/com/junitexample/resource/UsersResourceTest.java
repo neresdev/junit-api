e49035285f866da5d80f338fd76c2dd3d0ee1c8e
david.neres-at-422501651743 <david.neres-at-422501651743>
2023-02-07 16:13:37 -0300

    teste update na classe user resource

--- src/test/java/br/com/junitexample/resource/UsersResourceTest.java
+++ src/test/java/br/com/junitexample/resource/UsersResourceTest.java
@@ -113,7 +113,21 @@ class UsersResourceTest {
     }
 
     @Test
-    void update() {
+    void whenUpdateThenReturnSuccess() {
+        when(service.update(userDTO)).thenReturn(user);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<UsersDTO> response = resource.update(ID, userDTO);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UsersDTO.class, response.getBody().getClass());
+
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(NAME, response.getBody().getName());
+        assertEquals(EMAIL, response.getBody().getEmail());
     }
 
     @Test
